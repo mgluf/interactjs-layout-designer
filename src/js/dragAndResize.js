57e23be7820328996b61bfc052770602efcbc5a0
mgluf <gluf.matt@gmail.com>
2020-11-11 14:39:26 -0500

    PIVOT: boxes for fun

---
+++ src/js/dragAndResize.js
@@ -0,0 +1,114 @@
+const interact = require('interactjs')
+
+// create a restrict modifier to prevent dragging an element out of the canvas
+const restrictToCanvas = interact.modifiers.restrict({
+  restriction: 'parent',
+  elementRect: { left: 0, right: 1, top: 0, bottom: 1 }, //can alter this to create a 'margin' restriction
+})
+
+var drageAndResize =  interact('.resizable')
+  .draggable({
+    origin: 'parent',
+    modifiers: [restrictToCanvas],
+
+    listeners:{
+      start (event) {},
+      end (event) {},
+    }
+  })
+
+  .resizable({
+    modifiers: [restrictToCanvas],
+    listeners:{
+      start(event){
+        console.log(
+          JSON.stringify({
+            eventType: event.type,
+            objectID: event.target.id,
+            size:{
+              x: event.target.style.width,
+              y: event.target.style.height
+            }
+          }
+          ));
+      },
+
+      end(event){
+        console.log(
+          JSON.stringify({
+            eventType: event.type,
+            objectID: event.target.id,
+            size:{
+              x: event.target.style.width,
+              y: event.target.style.height
+            }
+          }
+          ));
+      }
+
+    },
+    preserveAspectRatio: false,
+    edges: {
+      left: true,
+      right: true,
+      bottom: true,
+      top: true
+    }
+
+  })
+  .on('dragstart', function (event) {
+    event.preventDefault();
+  })
+  .on('dragmove', dragMoveListener )
+
+  .on('resizemove', function(event) {
+    var target = event.target,
+      x = (parseFloat(target.getAttribute('data-x')) || 0),
+      y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+      'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+
+  })
+
+
+function dragMoveListener(event) {
+  var target = event.target,
+    // keep the dragged position in the data-x/data-y attributes
+    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+  // translate the element
+  target.style.webkitTransform =
+    target.style.transform =
+    'translate(' + x + 'px, ' + y + 'px)';
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+  
+}
+
+interact('.resizeable').draggable({
+  modifiers: [
+    interact.modifiers.snap({
+      targets: [ { x: 300, y: 300 } ],
+      relativePoints: [
+        { x: 0  , y: 0   },   // snap relative to the element's top-left,
+        { x: 0.5, y: 0.5 },   // to the center
+        { x: 1  , y: 1   }    // and to the bottom-right
+      ]
+    })
+  ]
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,154 +1,29 @@
-// =================================================================================
-// EDIT LAYOUT MODE
-// built using interact.js
-// https://interactjs.io/docs/
-// =================================================================================
+//init dragAndResize
+import dragAndResize from './dragAndResize.js'
+dragAndResize
 
-//FIXME: restrict resize to canvas
+//consider scrollbar for 100vw and 100vh for #canvas / .nav
+var windowHeight;
+(windowHeight = function considerScrollbar () {
+  const scrollbarWidth = window.innerWidth - document.body.clientWidth
 
-import editLayout from './editLayout'
+  let vwElements = Array.from(document.getElementsByClassName('100vw'))
+  let  vhElements = Array.from(document.getElementsByClassName('100vh'))
 
-document.getElementById("editLayoutButton").addEventListener("click", editLayout);
+  vwElements.forEach(child => {
+    console.log('hello')
+    child.style.width = `calc(100vw - ${scrollbarWidth}px)`
+  });
 
+  vhElements.forEach(child => {
+    child.style.height = `calc(100vh - ${scrollbarWidth}px)`
+  });
 
-const interact = require('interactjs')
+})();
 
-// create a restrict modifier to prevent dragging an element out of the canvas
-const restrictToCanvas = interact.modifiers.restrict({
-  restriction: 'parent',
-  elementRect: { left: 0, right: 1, top: 0, bottom: 1 }, //can alter this to create a 'margin' restriction
-})
+//==================================
+//
+// Box Selector
+//
+//==================================
 
-// create a snap modifier which changes the event coordinates to the closest corner of a grid
-const snap = interact.modifiers.snap({
-  targets: [interact.snappers.grid({ x: 50, y: 50 })],
-  relativePoints: [{ x: 1, y: 1 }],
-})
-
-interact('.resizable')
-  .draggable({
-    origin: 'parent',
-    modifiers: [restrictToCanvas, snap],
-
-    listeners:{
-      start (event) {
-        console.log(
-          JSON.stringify({
-            eventType: event.type,
-            objectID: event.target.id,
-          }
-          ));
-
-      },
-
-      end (event) {
-        console.log(
-          JSON.stringify({
-            eventType: event.type,
-            objectID: event.target.id,
-          }
-          ));
-      },
-  }
-
-  })
-  .resizable({
-    modifiers: [snap],
-    listeners:{
-      start(event){
-        console.log(
-          JSON.stringify({
-            eventType: event.type,
-            objectID: event.target.id,
-            size:{
-              x: event.target.style.width,
-              y: event.target.style.height
-            }
-          }
-          ));
-      },
-
-      end(event){
-        console.log(
-          JSON.stringify({
-            eventType: event.type,
-            objectID: event.target.id,
-            size:{
-              x: event.target.style.width,
-              y: event.target.style.height
-            }
-          }
-          ));
-      }
-
-    },
-    preserveAspectRatio: false,
-    edges: {
-      left: true,
-      right: '.resize-handle',
-      bottom: '.resize-handle',
-      top: true
-    }
-
-  })
-  .on('dragstart', function (event) {
-    event.preventDefault();
-  })
-  .on('dragmove', dragMoveListener )
-
-  .on('resizemove', function(event) {
-    var target = event.target,
-      x = (parseFloat(target.getAttribute('data-x')) || 0),
-      y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-    // update the element's style
-    target.style.width = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
-
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
-
-    target.style.webkitTransform = target.style.transform =
-      'translate(' + x + 'px,' + y + 'px)';
-
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-
-  })
-
-
-function dragMoveListener(event) {
-  var target = event.target,
-    // keep the dragged position in the data-x/data-y attributes
-    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-  // translate the element
-  target.style.webkitTransform =
-    target.style.transform =
-    'translate(' + x + 'px, ' + y + 'px)';
-
-  // update the posiion attributes
-  target.setAttribute('data-x', x);
-  target.setAttribute('data-y', y);
-  
-}
-
-interact('.resizeable').draggable({
-  modifiers: [
-    interact.modifiers.snap({
-      targets: [ { x: 300, y: 300 } ],
-      relativePoints: [
-        { x: 0  , y: 0   },   // snap relative to the element's top-left,
-        { x: 0.5, y: 0.5 },   // to the center
-        { x: 1  , y: 1   }    // and to the bottom-right
-      ]
-    })
-  ]
-})
-
-// =================================================================================
-// IMAGE CROPPING
-// 
-// =================================================================================
